6f3647b574114591cfd3641235baaf81c90c5008
mayukuno <kunomayu1006@gmail.com>
2020-08-11 18:52:53 +0900

    created message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200811093559) do
+ActiveRecord::Schema.define(version: 20200811095115) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "zipcode",         null: false
@@ -47,6 +47,17 @@ ActiveRecord::Schema.define(version: 20200811093559) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",    limit: 65535
+    t.text     "image",      limit: 65535
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "post_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -88,6 +99,8 @@ ActiveRecord::Schema.define(version: 20200811093559) do
   add_foreign_key "addresses", "users"
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
   add_foreign_key "post_categories", "categories"
   add_foreign_key "post_categories", "posts"
 end
