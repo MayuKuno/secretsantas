7ada99b72ba3655571a247622ef0621d35276830
mayukuno <kunomayu1006@gmail.com>
2020-09-19 00:44:23 +0900

    fixed db

--- db/migrate/20200814125131_create_group_users.rb
+++ db/migrate/20200814125131_create_group_users.rb
@@ -2,8 +2,7 @@ class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
       t.references :group, foreign_key: true
-      t.integer :participant_id
-      t.integer :drawn_id
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -33,11 +33,11 @@ ActiveRecord::Schema.define(version: 20200814125131) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
-    t.integer  "participant_id"
-    t.integer  "drawn_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_group_users_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_group_users_on_user_id", using: :btree
   end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20200814125131) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "group_users", "groups"
+  add_foreign_key "group_users", "users"
   add_foreign_key "messages", "groups"
   add_foreign_key "messages", "users"
   add_foreign_key "post_categories", "categories"
