e8453a3bc12f63c5aeff170a7bfc225ed95781c3
mayukuno <kunomayu1006@gmail.com>
2020-10-03 00:21:23 +0900

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,20 @@
 class PostsController < ApplicationController
   def index
-    # @posts = Post.all
-    # return nil if params[:keyword] == ""
-    # @categories = Category.where(['name LIKE ?', "%#{params[:keyword]}%"] ).limit(10)
-    # @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
-    # respond_to do |format|
-    #   format.html
-    #   format.json
-    # end
     @tags = Post.tag_counts_on(:tags).most_used(5)    # タグ一覧表示
-
     if params[:tag]
-      @posts = Post.tagged_with(params[:tag])
-      
+      @posts = Post.tagged_with(params[:tag])    
     else
       @posts = Post.all
     end
-
   end
+
   def show
     # @tag =  ActsAsTaggableOn::Tag.find(params[:id])
     # @posts = Post.tagged_with(@tag.name)
-
     # @tags = Post.tag_counts_on(:tags).order('count DESC')     # 全タグ(Postモデルからtagsカラムを降順で取得)
     # if @tag = params[:tag]   # タグ検索用
     #   @post = Post.tagged_with(params[:tag])   # タグに紐付く投稿
     # end
-
   end
 
   def new
