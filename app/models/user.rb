9f80cfca625f70398c46afebbebc625ca03b6133
mayukuno <kunomayu1006@gmail.com>
2020-10-27 22:58:01 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,7 @@ class User < ApplicationRecord
 
   def self.guest
 
-    find_or_create_by!(nickname: 'Mx.Geust', email: 'guest@example.com',first_name: 'GuestLastname',last_name: 'GuestLastname',birthday: '2010-10-10') do |user|
+    find_or_create_by!(nickname: 'Mx.Geust', email: 'guest@example.com',first_name: 'GuestFirstname',last_name: 'GuestLastname',birthday: '2010-10-10') do |user|
       user.password = SecureRandom.urlsafe_base64
 
     end
